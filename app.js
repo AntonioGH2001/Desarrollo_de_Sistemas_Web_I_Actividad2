f1e23e83fc9001ffccab6b3d9653c7a166050bd3
AntonioGH2001 <antoniogh259@gmail.com>
2026-05-27 17:10:33 -0600

    Archivos de actividad 2

---
+++ app.js
@@ -0,0 +1,18 @@
+var Alert = new CustomAlert();
+
+function CustomAlert(){
+
+    this.render = function(){
+        //Show Modal
+        let popUpbox =document.getElementById('popUpBox');
+        popUpbox.style.display = "block";
+        //Close Modal
+        document.getElementById('closeModal').innerHTML = '<a href="index.php"><button type="button" a href>¡Si quiero!</button></a>';
+
+    }
+
+    this.ok = function(){
+        document.getElementById('popUpBox').style.display = "none";
+        document.getElementById('popUpOverlay').style.display = "none";
+    }
+}
